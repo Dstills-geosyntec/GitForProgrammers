11c7cab601bf131f0ce642a2bccd6067276a26d0
Dstills-geosyntec <dstills@geosyntec.com>
2021-12-31 16:03:06 -0800

    Second commit - from Visual Studio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace GitForProgrmmers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, world!")
+			Console.WriteLine("I just added this in Visual Studio");
+		}
+	}
+}
